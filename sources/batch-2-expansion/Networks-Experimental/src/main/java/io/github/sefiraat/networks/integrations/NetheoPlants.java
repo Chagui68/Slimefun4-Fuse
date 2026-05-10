75aca9b6a141f5281b7c12329c9fedc033715e7a
Chagui68 <danielramirogt1821@gmail.com>
2026-05-10 12:32:33 -0500

    Networks Experimental added

---
+++ sources/batch-2-expansion/Networks-Experimental/src/main/java/io/github/sefiraat/networks/integrations/NetheoPlants.java
@@ -0,0 +1,27 @@
+package io.github.sefiraat.networks.integrations;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * NetheoPlants integration - Temporarily disabled due to import conflicts.
+ * TODO: Re-enable once Netheopoiesis is ported to Drake's Slimefun core.
+ */
+@UtilityClass
+public class NetheoPlants {
+
+    // TODO: Re-enable NetheoPlants integration
+    // public static void setup() {
+    //     // Integration code here
+    // }
+}
+
+
+
+
+
+
+
+
+
+
+
